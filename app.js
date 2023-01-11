78ef7a1c89d60d2c216e58372c6cf10ba7b5fd60
kn8a <mydreamhomefl@gmail.com>
2023-01-11 06:17:43 -0500

    connected DB

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var path = require('path');
+const dotenv = require("dotenv").config()
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const connectDb = require("./config/db")
+const cors = require("cors")
+
+connectDb()
 
 var users = require('./routes/users');
 
 var app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
-app.use('/api/v1/users', users);
+app.use('/api/users', users);
 
 module.exports = app;
