0bb621319b60900918cbdd315de0e9a76f5d1aad
kn8a <mydreamhomefl@gmail.com>
2023-02-03 00:52:55 -0500

    fix errors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const asyncHandler = require("express-async-handler")
 
 connectDb()
 
+var indexRouter = require('./routes/index');
 var users = require('./routes/users');
 var appData = require('./routes/appData')
 
@@ -18,14 +19,37 @@ const connected = asyncHandler(async (req,res) => {
     res.status(200).json({ message: 'connected' })
 })
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(cors())
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
 
 app.use('/api/connect', connected)
 
 app.use('/api/users', users);
 app.use('/api/appdata', appData)
+
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 module.exports = app;
